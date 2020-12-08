d266143dbd86b0f2882e26c9a5d1c7ad0ffb4c04
Leon Schmidt <58957741+schmidtleon48@users.noreply.github.com>
2020-12-08 10:07:20 +0100

    Update setup_einklinken.bash

--- setup_einklinken.bash
+++ setup_einklinken.bash
@@ -8,26 +8,6 @@ read ws
 echo "Geben Sie die Ip-Adresse des Masters an"
 read ip
 
-echo "Haben Sie schon ROS auf ihrem Ubuntu-PC?(y/n)"
-read ans1
-if [ ans1=n];then
-	sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-	sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-	sudo apt update
-	sudo apt install -y ros-noetic-desktop-full
-	source /opt/ros/noetic/setup.bash
-	echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
-	source ~/.bashrc
-	sudo apt-get install python3-rosdep
-	sudo rosdep init
-	rosdep update
-	mkdir catkin_ws
-	cd catkin_ws
-	mkdir src
-	catkin_make
-	source devel/setup.bash
-fi
-
 echo "Haben Sie schon das Wellenzahl-Package?(y/n)"
 read ans
 if [ ans=n];then

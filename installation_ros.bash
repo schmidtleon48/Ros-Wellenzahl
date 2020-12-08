d2cb194d7bf12fd5e35cf146d1a2311e5b3dea9c
Leon Schmidt <58957741+schmidtleon48@users.noreply.github.com>
2020-12-08 09:58:28 +0100

    Update installation_ros.bash

--- installation_ros.bash
+++ installation_ros.bash
@@ -8,10 +8,9 @@ read ben
 
 ssh $ben@$IP<<EOF
 
-
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
 sudo apt update
 
@@ -28,6 +27,8 @@ sudo rosdep init
 
 rosdep update
 
+cd
+
 mkdir catkin_ws
 
 cd catkin_ws
@@ -37,12 +38,4 @@ mkdir src
 catkin_make
 
 source devel/setup.bash
-
-cd src 
-
-catkin_create_pkg wellenzahl rospy
-
-cd
-
-scp -r ~/catkin_ws/src/wellenzahl/src $ben@$IP:~/catkin_ws/src/wellenzahl/
 EOF
